f6083c04490764bc36f46b053d2383503f74d301
kevindgzmn <kevindgzmn@gmail.com>
2021-04-03 17:52:57 +0800

    - Test repository

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <Banner/>
+
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOrigials}
